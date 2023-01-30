e3998df4ab9a01c1dc0bfd03b5c12e80edf1b0cb
Abbie <abigail@ient.me>
2023-01-30 11:10:07 +0000

    Fix & run datagen and remove resulting redundant files

--- src/main/java/de/luaxlab/shipping/data/DataGenerators.java
+++ src/main/java/de/luaxlab/shipping/data/DataGenerators.java
@@ -4,12 +4,18 @@ import de.luaxlab.shipping.data.client.ModBlockStateProvider;
 import de.luaxlab.shipping.data.client.ModItemModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraftforge.common.data.ExistingFileHelper;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public final class DataGenerators implements DataGeneratorEntrypoint {
 	@Override
     public void onInitializeDataGenerator(FabricDataGenerator gen){
-        ExistingFileHelper existingFileHelper = ExistingFileHelper.standard();
+		Path existingData = Path.of(System.getProperty("de.luaxlab.shipping.existingData"));
+        ExistingFileHelper existingFileHelper = ExistingFileHelper.withResources(existingData);
 
         gen.addProvider(true, new ModBlockStateProvider(gen, existingFileHelper));
         gen.addProvider(true, new ModItemModelProvider(gen, existingFileHelper));
